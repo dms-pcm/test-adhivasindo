f1c88f9cef61cbce21ab416470d201559de80fe7
dms-pcm <81421412+dms-pcm@users.noreply.github.com>
2025-02-05 11:47:26 +0700

    provide api auth

--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\API\AuthController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -14,6 +15,11 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
+Route::post('user/login', [AuthController::class, 'login']);
+
+Route::group(['middleware' => [
+    'auth:sanctum'
+]], function () {
+    Route::get('user-me', [AuthController::class, 'me']);
+    Route::get('logout', [AuthController::class, 'logout']);
 });
