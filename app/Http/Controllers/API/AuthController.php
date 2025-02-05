81ae21c1d7246351c50e9ae359226cfd6df593ea
dms-pcm <81421412+dms-pcm@users.noreply.github.com>
2025-02-05 11:47:00 +0700

    develop auth group

---
+++ app/Http/Controllers/API/AuthController.php
@@ -0,0 +1,100 @@
+<?php
+
+namespace App\Http\Controllers\API;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\JsonResponse;
+
+use App\Http\Resources\UserResource;
+use Illuminate\Http\Request;
+use App\Models\User;
+use Exception;
+
+class AuthController extends Controller
+{
+    public function me() : JsonResponse
+    {
+        try {
+            return response()->json(auth('sanctum')->user());
+        } catch (Exception $e) {
+            return response()->json([
+                'data' => null,
+                'status' => [
+                    'message' => $e->getMessage(),
+                    'code' => 400
+                ]
+            ], 400);
+        }
+    }
+    public function login(Request $request) : JsonResponse
+    {
+        $request->validate([
+            'nim' => 'required',
+            'password' => 'required'
+        ]);
+
+        auth()->attempt(['nim' => $request->nim, 'password' => $request->password]);
+
+        $user = auth('sanctum')->user();
+
+        if (!$user) {
+            return response()->json([
+                'data' => null,
+                'status' => [
+                    'message' => 'Nim or Password wrong.',
+                    'code' => 400
+                ]
+            ], 400);
+        }
+
+        try {
+            $token = $user->createToken(config('app.name'))->plainTextToken;
+
+            $collection = UserResource::make($user);
+
+            return response()->json([
+                'data' => [
+                    'user' => $collection,
+                    'token' => $token
+                ],
+                'status' => [
+                    'message' => 'User Login successfully.',
+                    'code' => 200
+                ]
+            ], 200);
+
+        } catch (Exception $e) {
+            return response()->json([
+                'data' => null,
+                'status' => [
+                    'message' => $e->getMessage(),
+                    'code' => 400
+                ]
+            ], 400);
+        }
+    }
+
+    public function logout() : JsonResponse
+    {
+        try {
+            $user = User::find(auth('sanctum')->id());
+            $user->tokens()->delete();
+        } catch (Exception $e) {
+            return response()->json([
+                'data' => 'Error on user on auth',
+                'status' => [
+                    'message' => $e->getMessage(),
+                    'code' => 400
+                ]
+            ], 400);
+        }
+
+        return response()->json([
+            'data' => null,
+            'status' => [
+                'message' => 'User Logout successfully.',
+                'code' => 200
+            ]
+        ], 200);
+    }
+}
